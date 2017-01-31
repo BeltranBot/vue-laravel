1026f043efc691548aaf4378ba78b53fe48b41ec
BeltranBot <carlosbeltrane@gmail.com>
2017-01-31 10:30:03 -0500

    initial commit

---
+++ front/src/components/product/Product.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="col-md-4">
+    <div class="thumbnail">
+      <img src="https://cdn2.iconfinder.com/data/icons/virtual-keyboard/512/barcode-ean-code-item-coding-product-512.png" alt="">
+      <div class="caption">
+        <h3>{{ product.name }}</h3>
+        <p>{{ product.price }} </p>
+        <p>
+          <a href="#" class="btn btn-default">
+            Wish List
+          </a>
+          <a href="#" class="btn btn-success">
+            Buy
+          </a>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    props: ['product']
+  }
+</script>
+<style>
+</style>
---
+++ front/src/components/product/Products.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="row">
+      <my-product
+        v-for="product in products"
+        :product="product">
+      </my-product>
+  </div>
+</template>
+<script>
+  import Product from './Product.vue'
+  export default {
+    data () {
+      return {
+        products: []
+      }
+    },
+    created () {
+      this.$http.get('api/products')
+          .then(response => {
+            this.products = response.body
+          })
+    },
+    components: {
+      'my-product': Product
+    }
+  }
+</script>
+<style></style>
