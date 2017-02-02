5c53aea140d8caf5a038a6f388b2d587c2b10cf7
BeltranBot <carlosbeltrane@gmail.com>
2017-02-02 14:02:39 -0500

    Added changes for the latest video

--- back/app/Http/Controllers/ProductsController.php
+++ back/app/Http/Controllers/ProductsController.php
@@ -7,8 +7,14 @@ use App\Product;
 
 class ProductsController extends Controller
 {
-  public function index() 
+  public function index()
   {
-    return Product::all();
+    return Product::orderBy('created_at', 'desc')->get();
+  }
+
+  public function store(Request $request)
+  {
+    $product = Product::create($request->all());
+    return $product;
   }
 }
